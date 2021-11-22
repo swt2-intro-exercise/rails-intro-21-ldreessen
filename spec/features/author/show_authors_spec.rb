cf629e93e7646dbefece5d51f8950498b53f1817
ldreessen <leonard.dreessen@student.hpi.uni-potsdam.de>
2021-11-22 02:01:34 +0100

    fixes #9

--- spec/features/author/show_authors_spec.rb
+++ spec/features/author/show_authors_spec.rb
@@ -4,4 +4,17 @@ describe "Show authors index page", type: :feature do
   it "should exist at 'authors_path' and render without an error" do
     visit authors_path
   end
+  it "should already contain information about a specific existing author" do
+    create :author
+    visit authors_path
+
+	expect(page).to have_text("Alan Turing")
+    expect(page).to have_text("http://wikipedia.de/Alan_Turing")
+  end
+  it "should have a link to the page of each existing author" do
+    author = create :author
+    visit authors_path
+
+    expect(page).to have_link "Details", href: author_path(author.id)
+  end
 end
