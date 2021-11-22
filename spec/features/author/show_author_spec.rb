83d3c031a93d5352d10d81d988e6716f5ed54bfb
ldreessen <leonard.dreessen@student.hpi.uni-potsdam.de>
2021-11-22 01:37:03 +0100

    fixes #6

---
+++ spec/features/author/show_author_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+author_Alan = Author.create(
+            first_name:"Alan",
+            last_name:"Turing",
+            homepage:"http://wikipedia.org/Alan_Turing")
+
+describe "Author page", :type => :feature do
+	it "Author page should display all authors" do
+        visit authors_path
+        expect(page).to have_text("Alan")
+        expect(page).to have_text("Turing")
+    end
+
+    it "Author page should display author details #7" do
+        # Tipp im Issue: FactoryBot.create :author
+        visit authors_path(@Alan)
+        expect(page).to have_text("Alan")
+        expect(page).to have_text("Turing")
+        expect(page).to have_text("http://wikipedia.org/Alan_Turing")
+    end
+
+end 
